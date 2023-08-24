0b03fd16f59b58aa6e02474a93c95102d18f8e9a
Eric Campbell <64181520+nozberlin76@users.noreply.github.com>
2023-08-23 20:52:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 
-/*const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/orderapi.prod.tastesofchicago.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/orderapi.prod.tastesofchicago.com/fullchain.pem')
-};*/
+const options = {
+ key: fs.readFileSync('/etc/letsencrypt/live/orderapi.prod.tastesofchicago.com/privkey.pem'),
+ cert: fs.readFileSync('/etc/letsencrypt/live/orderapi.prod.tastesofchicago.com/fullchain.pem')
+}; 
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -21,24 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-    next();
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ next();
 });
-
-//app.use(cors());
 
 app.use('/feed', feedRoutes);
 // app.use('/.well-known', feedRoutes);
 
 //app.listen(PORT);
 
-app.listen(PORT, () => {
-  console.info(`server started on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+// console.info(`server started on port ${PORT}`);
+// });
 
-// http.createServer(app).listen(80);
+//http.createServer(app).listen(80);
 // Create an HTTPS service identical to the HTTP service.
-// https.createServer(options, app).listen(443);
+https.createServer(options, app).listen(443);
